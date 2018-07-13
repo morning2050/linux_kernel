a398613459161273aa6f6a83bd1ef4b43aa860a8
molin2050 <531486448@qq.com>
2018-07-13 11:55:11 +0800

    first commit

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "first commit"
-git remote add origin git@github.com:molin2050/ubuntucore_linux.git
+git remote add origin git@github.com:morning2050/linux_kernel.git
 git push -u origin master
